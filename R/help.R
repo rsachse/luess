34475b1bea45685bee5ba3234a68cf9bc5f45fa2
Rene Sache <rene.sachse@googlemail.com>
2013-07-24 15:42:02 +0200

    initial commit

---
+++ R/help.R
@@ -0,0 +1,10 @@
+#' Estimation of Ecosystem Services from LPJmL Model Output
+#'
+#' Collection of methods to derive quantification of ecosystem services
+#' from LPJmL model output.
+#'
+#' @docType package
+#' @name luess
+#' @author Rene Sachse <rene.sachse@uni-potsdam.de>
+#' @aliases luess luess-package
+NULL
