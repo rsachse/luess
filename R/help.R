0792a21ba296020f76ff651f3f76ab0f9c5c2878
Rene Sache <rene.sachse@googlemail.com>
2013-07-29 20:26:19 +0200

    give references

--- R/help.R
+++ R/help.R
@@ -12,8 +12,15 @@ NULL
 #'
 #' @name cft_nutrients
 #' @docType data
-#' @references see FAO (2001) and \url{http://www.fao.org/docrep/003/x9892e/X9892e05.htm#P8217_125315} 
+#' @references See FAO (2001) and \url{http://www.fao.org/docrep/003/x9892e/X9892e05.htm#P8217_125315} 
 #' for kcal, proteins and fat; Wirsenius (2000) for dry weights.
+#' 
+#' Food and Agriculture Organization of the United Nations, Rome, 2001. 
+#' FOOD BALANCE SHEETS. A handbook.
+#' 
+#' Wirsenius, S., 2000. Human use of Land and Organic Materials: Modeling the
+#' Turnover of Biomass in the Global Food System. Dept. of Physical Resource
+#' Theory, Chalmers University of Technology and Goeteborg University, Goeteborg.
 #' @keywords data, nutrients, dry weight, crop functional types
 #' 
 #' 
