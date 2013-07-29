f424c6c624d0df41a6df9055414e70498a3546b3
Rene Sache <rene.sachse@googlemail.com>
2013-07-29 20:13:17 +0200

    added cft_nutrients.rda data for nutritional composition of cfts

--- R/help.R
+++ R/help.R
@@ -8,3 +8,13 @@
 #' @author Rene Sachse <rene.sachse@@uni-potsdam.de>
 #' @aliases luess luess-package
 NULL
+#' Nutritional composition and dry weight of crop functional types
+#'
+#' @name cft_nutrients
+#' @docType data
+#' @references see FAO (2001) and \url{http://www.fao.org/docrep/003/x9892e/X9892e05.htm#P8217_125315} 
+#' for kcal, proteins and fat; Wirsenius (2000) for dry weights.
+#' @keywords data, nutrients, dry weight, crop functional types
+#' 
+#' 
+NULL
---
+++ work/construct_crop_nutrient_table.R
@@ -0,0 +1,2 @@
+cft_nutrients <- read.csv2("cft-nutrient-contents.csv")
+save(cft_nutrients, file="cft_nutrients.rda")
