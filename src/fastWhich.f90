c0086a6614f4d0707d529f258e7917f677f659a7
Rene Sachse <rene.sachse@googlemail.com>
2014-03-13 01:57:35 +0100

    fortran90 which replacement. still crashing when used inside of translation function.

--- src/fastWhich.f90
+++ src/fastWhich.f90
@@ -1,26 +1,20 @@
-subroutine fastWhich(xCor, yCor, vecA, vecB, range, vecResult)
-
+subroutine fastWhich(xCor, yCor, vecA, vecB, rectRange, vecResult)
   implicit none
-
   real(kind=8) :: xCor
   real(kind=8) :: yCor
   real(kind=8), dimension(67420) :: vecA
   real(kind=8), dimension(67420) :: vecB
-  real(kind=8) :: range
+  real(kind=8) :: rectRange
   logical, dimension(67420) :: vecResult
-  
-  integer i
-
+  integer :: i
   real(kind=8) :: xl 
   real(kind=8) :: xu  
   real(kind=8) :: yl  
   real(kind=8) :: yu  
-
-  xl = xCor - range
-  xu = xCor + range
-  yl = yCor - range
-  yu = yCor + range 
-
+  xl = xCor - rectRange
+  xu = xCor + rectRange
+  yl = yCor - rectRange
+  yu = yCor + rectRange 
   do i = 1, 67420, 1
     if ( vecA(i) >= xl .and. vecA(i) <= xu .and. vecB(i) >= yl .and. vecB(i) <= yu) then
        vecResult(i) = .true.
@@ -28,5 +22,4 @@ subroutine fastWhich(xCor, yCor, vecA, vecB, range, vecResult)
        vecResult(i) = .false.
     end if
   end do
-
 end subroutine fastWhich
